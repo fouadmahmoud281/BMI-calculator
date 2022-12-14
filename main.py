2b9898131ed43a049c2bd45546ca028c70ee9ab7
Fouad Mahmoud <100407343+fouadmahmoud281@users.noreply.github.com>
2022-12-14 17:05:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+weight= input("enter your weight in kg :")
+height= input("enter your height in m :")
+BMI = float(weight) / float(height)**2
+BMI_as_string= str(BMI)
+print("your BMI " +BMI_as_string)
